4b9d34fe0def876f0e4b768e3cc21e86e7a78e24
SzlomoGoldstein <135057585+SzlomoGoldstein@users.noreply.github.com>
2023-12-19 18:09:51 +0100

    Add files via upload

---
+++ C#Projects/Kursy walut/Kursy walut/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Kursy_walut
+{
+    internal class Program
+    {
+
+        static Dictionary<string, Currency> GetCurrencies() 
+        {
+            return new Dictionary<string, Currency>
+            {
+                { "usd", new Currency("usd", "United States, Dollar", 1) },
+                { "eur", new Currency("eur", "Euro", 0.83975) },
+                { "gbp", new Currency("gbp", "British Pound", 0.74771) },
+                { "cad", new Currency("cad", "Canadian Dollar", 1.30724) },
+                { "inr", new Currency("inr", "Indian Rupee", 73.04) },
+                { "mxn", new Currency("mxn", "Mexican Peso", 21.7571) }
+            };
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, Currency> currencies = GetCurrencies();
+            Console.WriteLine("Check the rate for:");
+            string userInput = Console.ReadLine();
+
+            Currency selectedCurrency = null;
+            if (currencies.TryGetValue(userInput, out selectedCurrency))
+            {
+                Console.WriteLine($"Rate for USD to {selectedCurrency.FullName} is {selectedCurrency.Rate}");
+            }
+            else 
+            {
+                Console.WriteLine("Currency not found");
+            }
+            currencies.Remove("usd");
+        }
+    }
+}
