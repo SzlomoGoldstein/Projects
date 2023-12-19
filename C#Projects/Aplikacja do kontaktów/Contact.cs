40c584f68c8324020cc05e84964919a94dfa8522
SzlomoGoldstein <135057585+SzlomoGoldstein@users.noreply.github.com>
2023-12-19 16:26:18 +0100

    Add files via upload

---
+++ C#Projects/Aplikacja do kontaktów/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_do_kontaktów
+{
+    internal class Contact
+    {
+        public Contact(string name, string number) 
+        {
+            Name = name;
+            Number = number;
+        }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
